858db632c841519f9e6a07669aa640c9b3cb585c
steve passarelli <passarellism@gmail.com>
2023-01-13 18:23:35 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+
+function findMatching(array, string) {
+    return array.filter(name => name.toUpperCase() === string.toUpperCase())
+}
+function fuzzyMatch(array, firstLetter) {
+    return array.filter(name => name[0] === firstLetter[0])
+}
+
+function matchName(array, match) {
+    return array.filter(name => name.name === match)
+    
+}
